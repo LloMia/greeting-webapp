413a25e17858a25676d00a99ea1556cea1c63480
Loyiso <Loyiso@projectcodex.co>
2017-08-02 15:32:38 +0200

    counter changes

--- form.js
+++ form.js
@@ -2,7 +2,9 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 var app = express();
-
+var names = [];
+var count = 0;
+var namesGreeted = {};
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -14,19 +16,57 @@ app.engine('handlebars', exphbs({
 
 app.set('view engine', 'handlebars')
 app.get('/', function(req, res) {
-  res.render('index');
+    res.render('index');
 })
 
 app.post('/', function(req, res) {
-  var msg = "";
-    var name = req.body.name;
-    var info = 'Hello, ' + name;
-  var greetName = {
-    msg:info
-  }
+  var name = req.body.name;
+    var language = req.body.language;
+    var greetName = "";
 
-    res.render('index', greetName);
+
+        names.push(name);
+        if(namesGreeted[name] === undefined){
+          namesGreeted[name] = 1;
+
+        if (language === "english") {
+            count++;
+            greetName = "hello, " + name + "!";
+        } else if (language === "Setswana") {
+            count++;
+            greetName = "Dumela, " + name + "!";
+        } else if (language === "French") {
+            count++;
+            greetName = "Bonjour, " + name + "!";
+        }
+}
+    console.log(names);
+    res.render('index', {
+        msg: greetName
+    });
 });
+
+app.get('/greeted', function(req, res) {
+
+    res.render('index', {
+        names: names
+    })
+
+})
+
+app.get('/counter', function(req, res) {
+  var name = req.body.name;
+      function counter(input) {
+        return input == name
+    }
+    var countCal = names.filter(counter).length;
+    console.log(namesGreeted)
+
+    res.render('index', {counter : namesGreeted.length});
+})
+
+
+
 var server = app.listen(3000, function() {
     var host = server.address().address;
     var port = server.address().port;

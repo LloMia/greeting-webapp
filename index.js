7f5c68b59c4f1586dd8f7398de6945be3a9bbb8a
Loyiso <Loyiso@projectcodex.co>
2017-08-03 09:02:33 +0200

    adde process env

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.get('/counter', function(req, res) {
 
 
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening on port 3000!')

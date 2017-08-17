bdcc040c8530f3559c021be962a1f431ee65dfbd
Loyiso <Loyiso@projectcodex.co>
2017-08-17 12:04:09 +0200

    route changes

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ app.engine('handlebars', exphbs({
 
 app.set('view engine', 'handlebars');
 
-// app.get('/', function(req, res) {
-    // res.redirect('/index');
-// })
+app.get('/', function(req, res) {
+    res.redirect('/index');
+})
 app.get('/index/greeted', routes.indexes);
 app.get('/index', routes.greetLang);
 app.post('/index', routes.greetLang);
